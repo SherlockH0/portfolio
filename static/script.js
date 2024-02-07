160d258709bd7c5efb156ff60e1ec73ad0b3d144
SherlockH0 <ddmlf7@gmail.com>
2024-02-07 11:09:32 +0200

    Add hero section and hover effects

--- static/script.js
+++ static/script.js
@@ -94,3 +94,30 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+/*==================== MOUSE TRAILER ====================*/
+
+const trailer = document.getElementById('trailer')
+
+animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`
+  }
+
+  trailer.animate(keyframes, {
+    duration: 800,
+    fill: 'forwards'
+  })
+}
+
+window.onmousemove = e => {
+  const interactable = e.target.closest('a'),
+        interacting = interactable !== null;
+
+
+
+  animateTrailer(e, interacting)
+}
