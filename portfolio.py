2b7550505fbff4b6b4f063cc0eb01955a80a797c
SherlockH0 <ddmlf7@gmail.com>
2024-02-10 18:10:53 +0200

    Add project load from github

--- portfolio.py
+++ portfolio.py
@@ -1,12 +1,44 @@
 from flask import Flask, render_template, url_for
+import requests
+import json
 
 app = Flask(__name__)
+
+github_user = 'SherlockH0'
+
+project_names = [
+    'bookshelf',
+    'slime'
+]
+
+
+def get_projects_from_github():
+    projects = []
+
+    for project in project_names:
+        url = 'https://raw.githubusercontent.com/' + github_user + \
+            '/' + project + '/main/description.json'
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print('Could not load', project, 'project :(')
+            continue
+
+        project = json.loads(response.text)
+        projects.append(project)
+
+    return projects
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html')
+
+    return render_template(
+        'index.html',
+        projects=get_projects_from_github()
+    )
 
 
 if __name__ == '__main__':
