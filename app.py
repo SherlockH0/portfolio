4774b7e08dbd8adbbc8defef03833575c61f37ad
SherlockH0 <ddmlf7@gmail.com>
2024-09-06 12:43:59 +0300

    Update

--- app.py
+++ app.py
@@ -1,92 +1,100 @@
-from flask import Flask, render_template, url_for, request, redirect
-import requests
 import json
 import os
+
+import requests
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
 
 mail = Mail(app)
 
-github_user = 'SherlockH0'
-my_email = 'daniil.davtian@gmail.com'
+github_user = "SherlockH0"
+my_email = "daniil.davtian@gmail.com"
 
-project_names = [
-    'bookshelf',
-]
+project_names = []
 
 
 def get_projects_from_github():
     projects = []
 
     for project_name in project_names:
-        url = 'https://raw.githubusercontent.com/' + github_user + \
-            '/' + project_name + '/main/description.json'
+        url = (
+            "https://raw.githubusercontent.com/"
+            + github_user
+            + "/"
+            + project_name
+            + "/main/description.json"
+        )
 
         try:
             response = requests.get(url)
+            if response.status_code != 200:
+                print("Could not load", project_name, "project :(")
+                continue
+
+            project = json.loads(response.text)
+            projects.append(project)
         except Exception as e:
-            print(e, '\noccurred loading project', project_name)
-
-        if response.status_code != 200:
-            print('Could not load', project_name, 'project :(')
-            continue
-
-        project = json.loads(response.text)
-        projects.append(project)
+            print(e, "\noccurred loading project", project_name)
 
     return projects
 
 
 def send_email():
-    sender_email = request.json.get('email', 'noreply@demo.com')
-    message_text = request.json.get('message', '')
-    sender_name = request.json.get('name', 'none')
+    if request.json:
+        sender_email = request.json.get("email", "noreply@demo.com")
+        message_text = request.json.get("message", "")
+        sender_name = request.json.get("name", "none")
 
-    msg = Message(
-        f'{sender_name} has a message for you',
-        sender=sender_email,
-        recipients=[my_email])
+        msg = Message(
+            f"{sender_name} has a message for you",
+            sender=sender_email,
+            recipients=[my_email],
+        )
 
-    kwargs = {'sender_email': sender_email,
-              'message_text': message_text,
-              'sender_name': sender_name}
+        kwargs = {
+            "sender_email": sender_email,
+            "message_text": message_text,
+            "sender_name": sender_name,
+        }
 
-    msg.body = render_template('email.txt', **kwargs)
-    msg.html = render_template('email.html', **kwargs)
+        msg.body = render_template("email.txt", **kwargs)
+        msg.html = render_template("email.html", **kwargs)
 
-    mail.send(msg)
+        mail.send(msg)
+
+        return "Message sent!", 200
+    else:
+        return "Error", 400
 
 
+projects = ""
 projects = get_projects_from_github()
 
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def home():
     return render_template(
-        'index.html',
+        "index.html",
         projects=projects,
         base_url=request.base_url,
         host=request.host,
-        my_email=my_email
+        my_email=my_email,
     )
 
 
-@app.route("/send_message/", methods=['POST'])
+@app.route("/send_message/", methods=["POST"])
 def send_message():
-    send_email()
-
-    return {
-        'message': 'Message sent!'
-    }
+    return send_email()
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
--- static/script.js
+++ static/script.js
@@ -33,186 +33,200 @@ const toggleMode = () => {
 };
 
 window
-.matchMedia("(prefers-color-scheme: dark)")
-.addEventListener("change", (event) => {
-  if (lightMode === null) {
-    if (event.matches) {
-      disableLightMode();
-    } else {
-      enableLightMode();
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    if (lightMode === null) {
+      if (event.matches) {
+        disableLightMode();
+      } else {
+        enableLightMode();
+      }
     }
-  }
-});
+  });
 /*==================== PRELOADER ====================*/
 
-const preloader = document.getElementById('preloader'),
-  preloaderText = document.getElementById('preloader__text')
+const preloader = document.getElementById("preloader"),
+  preloaderText = document.getElementById("preloader__text");
 
-
-history.scrollRestoration = 'manual'
+history.scrollRestoration = "manual";
 window.scroll({
   top: 0,
-  behavior: 'instant'
+  behavior: "instant",
 });
 
-Pace.on('progress', (progress) => {
-  console.log(progress)
-  preloaderText.innerHTML = ((progress) / 100).toFixed(2)
+Pace.on("progress", (progress) => {
+  console.log(progress);
+  preloaderText.innerHTML = (progress / 100).toFixed(2);
 });
 
 Pace.on("done", () => {
   const tl = gsap.timeline();
-  tl
-    .to('#preloader__text', {
+  tl.to(
+    "#preloader__text",
+    {
       y: 100,
-      ease: 'power2.in',
+      ease: "power2.in",
       onComplete() {
-        preloader.classList.add('preloader--hide')
-      }
-    }, 0.5)
-    .to('.header', {
-      y: 0,
-      ease: 'power1.out',
-    }, '<')
-    .to('.hero .slide-out span', {
+        preloader.classList.add("preloader--hide");
+      },
+    },
+    0.5,
+  )
+    .to(
+      ".header",
+      {
+        y: 0,
+        ease: "power1.out",
+      },
+      "<",
+    )
+    .to(".hero .slide-out span", {
       y: 0,
       stagger: 0.05,
-      ease: 'power4.out',
+      ease: "power4.out",
       duration: 1,
-      opacity: 1
+      opacity: 1,
     })
-    .to('.hero h2, .blob-container', {
-      y: 0,
-      ease: 'power1.out',
-      opacity: 1
-    }, '<0.2')
-    .to('body', {
-      overflowY: 'auto'
-    }, '<')
-})
+    .to(
+      ".hero h2, .blob-container",
+      {
+        y: 0,
+        ease: "power1.out",
+        opacity: 1,
+      },
+      "<0.2",
+    )
+    .to(
+      "body",
+      {
+        overflowY: "auto",
+      },
+      "<",
+    );
+});
 
 /*==================== MOUSE TRAILER ====================*/
 
-const touchDevice = (navigator.maxTouchPoints & 0xFF || 'ontouchstart' in document.documentElement);
+const touchDevice =
+  navigator.maxTouchPoints & 0xff || "ontouchstart" in document.documentElement;
 
-const trailer = document.getElementById('trailer');
+const trailer = document.getElementById("trailer");
 trailer.dataset.type = "";
 let big = false;
 let x = 0,
-    y = 0
+  y = 0;
 
-trailer.style.display = touchDevice ? 'none' : 'revert'
+trailer.style.display = touchDevice ? "none" : "revert";
 
 animateTrailer = (e, interacting) => {
   x = e.clientX - trailer.offsetWidth / 2;
   y = e.clientY - trailer.offsetHeight / 2;
   const keyframes = {
-    transform: `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`
-  }
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`,
+  };
 
   trailer.animate(keyframes, {
     duration: 800,
-    fill: 'forwards'
-  })
-}
+    fill: "forwards",
+  });
+};
 
 scaleTrailer = () => {
   const keyframes = {
-    transform: `translate(${x}px, ${y}px) scale(6)`
-  }
+    transform: `translate(${x}px, ${y}px) scale(6)`,
+  };
 
   trailer.animate(keyframes, {
     duration: 800,
-    fill: 'forwards'
-  })
-}
-
-window.onmousemove = touchDevice ? null : (e) => {
-  const interactable = e.target.closest("a, .interactable, button"),
-  interacting = interactable !== null || big;
-
-  animateTrailer(e, interacting);
-
-  trailer.dataset.type = big
-  ? trailer.dataset.type
-  : interacting
-  ? interactable.dataset.type
-  : "";
+    fill: "forwards",
+  });
 };
+
+window.onmousemove = touchDevice
+  ? null
+  : (e) => {
+      const interactable = e.target.closest("a, .interactable, button"),
+        interacting = interactable !== null || big;
+
+      animateTrailer(e, interacting);
+
+      trailer.dataset.type = big
+        ? trailer.dataset.type
+        : interacting
+          ? interactable.dataset.type
+          : "";
+    };
 
 /*==================== COPY ====================*/
 
-const copyLinks = document.querySelectorAll('.copy')
+const copyLinks = document.querySelectorAll(".copy");
 
 copyLinks.forEach((el) => {
-  el.addEventListener('click', () => {
+  el.addEventListener("click", () => {
     navigator.clipboard.writeText(el.innerHTML);
 
-    el.classList.add('copied')
-
-  })
-  el.addEventListener('mouseout', () => {
+    el.classList.add("copied");
+  });
+  el.addEventListener("mouseout", () => {
     setTimeout(() => {
-      el.classList.remove('copied')
+      el.classList.remove("copied");
     }, 300);
-  })
-})
+  });
+});
 
 /*==================== GSAP ====================*/
 
-gsap.utils.toArray('.scroll-animate').forEach((element, i) => {
+gsap.utils.toArray(".scroll-animate").forEach((element, i) => {
   gsap.to(element, {
     scrollTrigger: {
       trigger: element,
-      start: 'top 80%',
+      start: "top 80%",
     },
     opacity: 1,
     y: 0,
     duration: 1.5,
-    ease: 'power4.out'
-  })
-})
+    ease: "power4.out",
+  });
+});
 
-gsap.utils.toArray('.scroll-fade-in').forEach((element, i) => {
+gsap.utils.toArray(".scroll-fade-in").forEach((element, i) => {
   gsap.to(element, {
     scrollTrigger: {
       trigger: element,
-      start: 'bottom bottom'
+      start: "bottom bottom",
     },
     opacity: 1,
-    ease: 'power4.in',
-  })
-})
+    ease: "power4.in",
+  });
+});
 
-gsap.utils.toArray('.slide-out h1').forEach((element) => {
+gsap.utils.toArray(".slide-out h1").forEach((element) => {
   gsap.to(element, {
     scrollTrigger: {
       trigger: element,
-      start: 'bottom 80%',
+      start: "bottom 80%",
     },
     delay: 0.2,
     opacity: 1,
     y: 0,
-    ease: 'power4.out',
+    ease: "power4.out",
     duration: 1,
-  })
-})
-
+  });
+});
 
 /*==================== MOBILE MENU ====================*/
 menuButton = document.querySelector(".hamburger");
 mobileNav = document.querySelector(".mobile-nav");
 
 const toggleNav = () => {
-  const toExpand = (mobileNav.dataset.visible == "false")
-  mobileNav.dataset.visible = toExpand
-  menuButton.setAttribute('aria-expanded', toExpand)
-  gsap.to('main', {
-    filter: `blur(${toExpand ? '10px' : '0px'})`
-  })
-}
+  const toExpand = mobileNav.dataset.visible == "false";
+  mobileNav.dataset.visible = toExpand;
+  menuButton.setAttribute("aria-expanded", toExpand);
+  gsap.to("main", {
+    filter: `blur(${toExpand ? "10px" : "0px"})`,
+  });
+};
 
 menuButton.addEventListener("click", toggleNav);
 
-document.getElementById('mobile-contact')
-.addEventListener('click', toggleNav)
+document.getElementById("mobile-contact").addEventListener("click", toggleNav);
